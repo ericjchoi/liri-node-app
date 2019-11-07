ae5c9064e8a862678f860b17479e4fd347fe26b4
Eric Choi <39499807+ericjchoi@users.noreply.github.com>
2019-11-07 15:05:48 -0800

    LIRI nodejs app

--- README.md
+++ README.md
@@ -1,20 +1,70 @@
 # liri-node-app
 LIRI is a command line node app that takes in parameters and give search result back using API.
 
-### Files
-liri.js - main javascript file containing functions
-keys.js - conveys spotify API key information from .env to liri.js
-.env - contains spotify API keys
+## Files
+- liri.js - main javascript file containing functions
+- keys.js - conveys spotify API key information from .env to liri.js
+- .env - contains spotify API keys
+- random.txt - contains series of pairs of specific command and its objective valuse separated by comma
 
-### Execting app
-node liri.js concert-this <artist_name>
-node liri.js spotify-this-song <song_name>
-node liri.js movie-this <movie_name>
-node liri.js do-what-it-says
+## Execting app in command line
+- node liri.js concert-this artist_name
+- node liri.js spotify-this-song song_name
+- node liri.js movie-this movie_name
+- node liri.js do-what-it-says
 
-### Current progress
-concert-this function completed.
-spotify-this-song function completed.
+## Screenshots when the app running in command line
+![figure1](./images/concert_this_bts_nodejs_screenshot.jpg)
+Figure 1. Running "concert-this" command to search upcoming concert information of "bts" using "Bands in town API". npm axios used. Searched concert information includes
+* Name of the venue
+* Venue location
+* Date of the event
 
-Currently working on movie-this function.
-README.md file will be updated soon.
+![figure2](./images/spotify_this_song_man_in_the_mirror_nodejs_screenshot.jpg)
+Figure 2. Running "spotify-this-song" command to search song information of "man in the mirror" using "Spotify API". npm node-spotify-api used. Searched information includes
+* Artist(s)
+* Song name
+* Album title
+* Preview URL
+
+![figure3](./images/movie_this_the_matrix_nodejs_screenshot.jpg)
+Figure 3. Running "movie-this" command to search movie information of "the matrix" using "OMDB API". npm axios used. Searched information includes 
+* Title of the movie
+* Year the movie came out
+* IMDB Rating of the movie
+* Rotten Tomatoes Rating of the movie
+* Country where the movie was produced
+* Language of the movie
+* Plot of the movie
+* Actors in the movie
+
+![figure4](./images/movie_this_nodejs_screenshot.jpg)
+Figure 4. Running "movie-this" command to search movie information of default movie title "Mr. Nobody" when movie title was omitted on command line. "OMDB API" used. npm axios used. Searched information includes 
+* Title of the movie
+* Year the movie came out
+* IMDB Rating of the movie
+* Rotten Tomatoes Rating of the movie
+* Country where the movie was produced
+* Language of the movie
+* Plot of the movie
+* Actors in the movie
+
+![figure5](./images/do_what_it_says1_nodejs_screenshot.jpg)
+Figure 5. Running "do-what-it-says" command which runs specific sub-command after reading content of "random.txt" which contains pair of sub-command and objective value separated by comma. In this case, the content read by app was 
+
+* spotify-this-song,"I Want it That Way"
+
+![figure6](./images/do_what_it_says2_nodejs_screenshot.jpg)
+Figure 6. Running "do-what-it-says" command which runs specific sub-command after reading content of "random.txt" which contains pair of sub-command and objective value separated by comma. In this case, the content read by app was 
+
+* movie-this,"The Matrix"
+
+![figure7](./images/do_what_it_says3_nodejs_screenshot.jpg)
+Figure 7. Running "do-what-it-says" command which runs specific sub-command after reading content of "random.txt" which contains pair of sub-command and objective value separated by comma. In this case, the content read by app was 
+
+* concert-this,"BTS"
+
+## GitHub repository
+* https://github.com/ericjchoi/liri-node-app
+
+## Programmed by Eric Choi
--- liri.js
+++ liri.js
@@ -6,62 +6,167 @@ var keys = require("./keys");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-
-// in order to use function #1: concertThis()
-var axios = require("axios");
-var moment = require("moment");
+var axios = require("axios");   // concert-this, movie-this
+var moment = require("moment"); // concert-this
+var targetValue = "";           // do-what-it-says
 
 // function #1
 function concertThis() {
-    var artist = process.argv.slice(3);
+    if (targetValue === "") {
+        var artist = process.argv.slice(3);
+    } else {
+        var artist = targetValue;
+    }
     axios
         .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
         .then(function (response) {
             for (var i = 0; i < response.data.length; i++) {
                 console.log(
-                    "\n+------------------------------------------------------------------\n" +
+                    "\n+-----------------------------------------------------------------------\n" +
                     "Name of the venue : " + response.data[i].venue.name + "\n" +
                     "Venue location    : " + response.data[i].venue.city + "," + response.data[i].venue.country + "\n" +
                     "Date of the event : " + moment(response.data[i].datetime).format("L") + "\n" +
-                    "+------------------------------------------------------------------\n");
+                    "+-----------------------------------------------------------------------\n");
             }
         })
         .catch(function (err) {
-            console.log("\n+------------------------------------------------------------------");
+            console.log("\n+-----------------------------------------------------------------------");
             console.log("ERROR : Please review the following error message and re-try.");
             console.log("        Please re-enter the artist(s) correctly.\n\n");
             console.log(err);
-            console.log("\n+------------------------------------------------------------------\n");
-
+            console.log("\n+-----------------------------------------------------------------------\n");
         });
 }
 
 // function #2
 function spotifyThisSong() {
-    var song = process.argv.slice(3);
+    if (targetValue === "") {
+        var song = process.argv.slice(3);
+    } else {
+        var song = targetValue;
+    }
     spotify
         .search({ type: "track", query: song })
         .then(function (response) {
-                console.log("\n+------------------------------------------------------------------");
-                console.log("Artist(s)   : " + response.tracks.items[0].artists[0].name);
-                console.log("Song name   : " + response.tracks.items[0].name);
-                console.log("Album title : " + response.tracks.items[0].album.name);
-                console.log("Preview URL : " + response.tracks.items[0].preview_url);
-                console.log("+------------------------------------------------------------------\n");
+            console.log("\n+-----------------------------------------------------------------------");
+            console.log("Artist(s)   : " + response.tracks.items[0].artists[0].name);
+            console.log("Song name   : " + response.tracks.items[0].name);
+            console.log("Album title : " + response.tracks.items[0].album.name);
+            console.log("Preview URL : " + response.tracks.items[0].preview_url);
+            console.log("+-----------------------------------------------------------------------\n");
+            globalWait = false;
         })
         .catch(function (err) {
             console.log("ERROR : Please review the following error message and re-try.");
             console.log("        Please re-enter the song title correctly.\n\n");
             console.log(err);
-            console.log("\n+------------------------------------------------------------------\n");
+            console.log("\n+-----------------------------------------------------------------------\n");
         });
 }
+
+// function #3
 function movieThis() {
-    console.log("3");
+    if (targetValue === "") {
+        var movieName = "";
+        if (process.argv.length >= 4) {          // movie title entered
+            movieName = process.argv.slice(3);
+        } else {                                 // movie title not entered
+            movieName = "Mr. Nobody";
+        }
+    } else {
+        var movieName = targetValue;
+    }
+    axios
+        .get("http://www.omdbapi.com/?apikey=trilogy&t=" + movieName)
+        .then(function (response) {
+            console.log("\n+-----------------------------------------------------------------------");
+            console.log("Title of the movie      : " + response.data.Title);
+            console.log("Year the movie came out : " + response.data.Year);
+            console.log("IMDB Rating             : " + response.data.Ratings[0].Value);
+            console.log("Rotten Tomatoes Rating  : " + response.data.Ratings[1].Value);
+            console.log("Country of production   : " + response.data.Country);
+            console.log("Language of the movie   : " + response.data.Language);
+            console.log("Actors in the movie     : " + response.data.Actors);
+            console.log("Plot of the movie       : " + response.data.Plot);
+            console.log("+-----------------------------------------------------------------------\n");
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 function doWhatItSays() {
-    console.log("4");
+    var fs = require("fs");
+
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+        // console.log("\n+-----------------------------------------------------------------------");
+        // console.log('Content of "random.txt" file : \n' + data);
+
+        // Carrage return should not be included in the content of the random.txt file.
+        // Content of "random.txt"
+        // spotify-this-song,"I Want it That Way",movie-this,"The Matrix",concert-this,"BTS"
+
+        // (1) Processing one API search
+        var j = 4; // manually select j. 0: first process, 2: second process, 4: third process
+        switch (dataArr[j]) {
+            case "concert-this":
+                targetValue = dataArr[j + 1].slice(1, dataArr[j + 1].length - 1);
+                concertThis();
+                targetValue = "";
+                break;
+            case "spotify-this-song":
+                targetValue = dataArr[j + 1].slice(1, dataArr[j + 1].length - 1);
+                spotifyThisSong();
+                targetValue = "";
+                break;
+            case "movie-this":
+                targetValue = dataArr[j + 1].slice(1, dataArr[j + 1].length - 1);
+                movieThis();
+                targetValue = "";
+                break;
+            default:
+                console.log(" ");
+        }
+
+        // (2) Processing multiple API search
+        // Following for-loop works fine but the response sequence is ASYNCHRONOUS
+        // not following the process sequence of the content of random.txt
+        // for (var j = 0; j < dataArr.length; j += 2) {
+        //     switch (dataArr[j]) {
+        //         case "concert-this":
+        //             targetValue = dataArr[j + 1].slice(1, dataArr[j + 1].length - 1);
+        //             console.log(dataArr[j]);
+        //             console.log(targetValue);
+        //             concertThis();
+        //             targetValue = "";
+        //             break;
+        //         case "spotify-this-song":
+        //             targetValue = dataArr[j + 1].slice(1, dataArr[j + 1].length - 1);
+        //             console.log(dataArr[j]);
+        //             console.log(targetValue);
+        //             spotifyThisSong();
+        //             targetValue = "";
+        //             break;
+        //         case "movie-this":
+        //             targetValue = dataArr[j + 1].slice(1, dataArr[j + 1].length - 1);
+        //             console.log(dataArr[j]);
+        //             console.log(targetValue);
+        //             movieThis();
+        //             targetValue = "";
+        //             break;
+        //         default:
+        //             console.log(" ");
+        //     }
+        // }
+
+    });
 }
+
 switch (process.argv[2]) {
     case "concert-this":
         concertThis();
@@ -74,4 +179,7 @@ switch (process.argv[2]) {
         break;
     case "do-what-it-says":
         doWhatItSays();
+        break;
+    default:
+        console.log(" ");
 }
